fd3887b172984198578f28ef9113d0827c368775
strawberrywang <128673200+strawberrywang@users.noreply.github.com>
2023-06-09 16:20:36 +0800

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,65 @@
+// console.log("jonas");
+// console.log(23);
+// console.log(40+8+23-10);
+// let firstname = "jonas";
+// let firstName = "jonas";
+// let javascript 
+// console.log(43);
+// // don't start with a number
+// //data types
+// //value Objec/primitive value 
+// console.log(firstName);
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: 
+BMI = mass / height ** 2 = mass / (height * height). 
+(mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
+//data 1
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+//data 2
+// const markHeight = 1.88;
+// const markMass = 95;
+// const johnHeight = 1.76;
+// const johnMass = 85;
+
+// const markBMI = markMass / markHeight ** 2;
+// const johnBMI = johnMass / johnHeight ** 2;
+// const markHigherBMI = markBMI >johnBMI;
+
+// if (markHigherBMI){
+//     console.log(`mark's BMI(${markBMI}) is higher than John's(${johnBMI}) ! `)
+// }else{
+//     console.log(`John's BMI (${johnBMI})  is higher than Mark's (${markBMI}) ! `)
+// }
+
+// console.log(markBMI,johnBMI,markHigherBMI);
+
+
+// const firstName = 'jonas';
+// const jonasNew = `I'm ${firstName}.`;
+// console.log(jonasNew);
+// console.log('string with \n\ multiple \n\ lines');
+
+// const age = 17;
+// const isOldEnough = age >= 18;
+// if (isOldEnough) {
+//     console.log('Sarah can start driving license.')
+// }else{
+//     console.log("Sarah can't start driving license.")
+// }
+
